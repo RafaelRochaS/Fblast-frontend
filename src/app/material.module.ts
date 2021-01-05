b5772542747159e4125295a9587a36ae924523ff
Rafael Souza <rafaelrochasouza@outlook.com.br>
2021-01-05 15:49:10 -0300

    Working on task

---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const modules = [
+  MatToolbarModule
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: modules,
+  exports: modules,
+})
+export class MaterialModule { }
