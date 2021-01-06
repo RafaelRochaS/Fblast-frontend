d92d2717c5372f7a226b3856050598a2588a43f5
Rafael Souza <rafaelrochasouza@outlook.com.br>
2021-01-06 14:57:36 -0300

    Working on task

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 const modules = [
-  MatToolbarModule
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatTooltipModule,
+  MatCardModule,
+  MatInputModule
 ];
 
 
