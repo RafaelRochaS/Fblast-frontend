4e34060dd3a2ccc8c889fd5915c4c4fecb1938d9
Rafael Souza <rafaelrochasouza@outlook.com.br>
2021-01-14 14:52:29 -0300

    Fixed test

--- src/app/registration/registration-home/registration-home.component.spec.ts
+++ src/app/registration/registration-home/registration-home.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/material.module';
 
 import { RegistrationHomeComponent } from './registration-home.component';
 
@@ -8,7 +11,13 @@ describe('RegistrationHomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegistrationHomeComponent ]
+      declarations: [ RegistrationHomeComponent ],
+      imports: [
+        MaterialModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));
