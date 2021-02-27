03dc9918f374cfa2a418dd6494efdfaa6bcadf7a
Rafael Souza <rafaelrochasouza@outlook.com.br>
2021-02-27 18:46:17 -0300

    Task22 advanced

--- src/app/logged/expenses/expenses.component.ts
+++ src/app/logged/expenses/expenses.component.ts
@@ -4,15 +4,14 @@ import { IUser } from '../../models/userData.model';
 import { UserDataService } from '../../services/user-data.service';
 
 @Component({
-    selector: 'app-logged-home',
-    templateUrl: './logged-home.component.html',
-    styleUrls: ['./logged-home.component.css']
+    selector: 'app-expenses',
+    templateUrl: './expenses.component.html',
+    styleUrls: ['./expenses.component.css']
 })
-export class LoggedHomeComponent implements OnInit {
+export class ExpensesComponent implements OnInit {
 
     currentUser$: Observable<IUser>;
     displayedColumns: string[] = ['name', 'value', 'date'];
-
 
     constructor(private userDataService: UserDataService) {
         this.currentUser$ = this.userDataService.currentUser$;
@@ -21,7 +20,4 @@ export class LoggedHomeComponent implements OnInit {
     ngOnInit(): void {
     }
 
-    /*     geTotalExpense(): number {
-            return this.expensesTable.map(i => i.value).reduce((acc, value) => acc + value, 0);
-        } */
 }
