a18b6f3cd85214dc1a44043bba5d232f61128fa7
Rafael Souza <rafaelrochasouza@outlook.com.br>
2021-03-18 23:46:00 -0300

    Tests fixed

--- src/app/logged/logged-home/logged-home.component.spec.ts
+++ src/app/logged/logged-home/logged-home.component.spec.ts
@@ -1,4 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/material.module';
 
 import { LoggedHomeComponent } from './logged-home.component';
 
@@ -8,7 +11,13 @@ describe('LoggedHomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoggedHomeComponent ]
+      declarations: [ LoggedHomeComponent ],
+      imports: [
+        MaterialModule,
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));
